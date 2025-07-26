62e13c65d40f6d42e25ee692f42229ba4b377e3d
IlkaCarmo <ilka.scarmo@gmail.com>
2025-07-25 22:10:00 -0300

    implementação positional-only

--- DesafioComFuncoesBanco.py
+++ DesafioComFuncoesBanco.py
@@ -1,9 +1,11 @@
 menu = """
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+[1] Cadastrar usuário
+[2] Cadastrar conta
+[3] Depositar
+[4] Sacar
+[5] Extrato
+[6] Sair
 
 => """
 
@@ -13,13 +15,8 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
-def saque(valor):
-    global saldo
-    global numero_saques
-    global limite
-    global LIMITE_SAQUES
-    global extrato
-
+def saque(valor,saldo,numero_saques,limite,LIMITE_SAQUES,extrato):
+    
     excedeu_saldo = valor >  saldo
 
     excedeu_limite = valor > limite
@@ -39,46 +36,43 @@ def saque(valor):
          saldo -= valor
          extrato += f"Saque: R$ {valor:.2f}\n"
          numero_saques += 1
-         return print("Saque realizado com sucesso")
-
+         return print(f"Saque realizado com sucesso!\n Saldo: R$ {saldo:.2f}\n================ EXTRATO ================ \n{extrato}")
+                
     else:
          return print("Operação falhou! O valor informado é inválido.")
 
-def deposito(valor):
-     global extrato
-     global saldo
+def deposito(valor,extrato,saldo):
 
      if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
-            return print("Deposito realizado com sucesso.")
+            return print(f"Deposito realizado com sucesso!\n Saldo: R$ {saldo:.2f}\n================ EXTRATO ================ \n{extrato}")
      else:
          return print("Operação falhou! O valor informado é inválido.")
 
-def extrato_da_conta():
-      global extrato
-      global saldo
+def extrato_da_conta(saldo, /, extrato):
 
       print("\n================ EXTRATO ================")
       print("Não foram realizadas movimentações." if not extrato else extrato)
       print(f"\nSaldo: R$ {saldo:.2f}")
       print("==========================================")
 
+
 while True:
     opcao = input(menu)
 
-    if opcao == "d":
+    if opcao == "3":
         valor = float(input("Informe o valor do depósito: "))
-        deposito(valor)
+        deposito(valor,extrato,saldo)
 
-    elif opcao == "s":
+    elif opcao == "4":
         valor = float(input("Informe o valor do saque: "))
-        saque(valor)
+        saque(valor=valor,saldo =saldo,numero_saques=numero_saques,limite=limite,LIMITE_SAQUES=LIMITE_SAQUES,extrato=extrato)
 
-    elif opcao == "e":
-        extrato_da_conta()
-
-    elif opcao == "q":
+    elif opcao == "5":
+        extrato_da_conta(saldo, extrato= extrato)
+         
+    elif opcao == "6":
         break
 
     else:
